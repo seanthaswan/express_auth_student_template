4b2b1bc820eb61515eda3a0b9d879c9203ead41f
Sean Swanson <sean-swan@hotmail.com>
2018-06-27 18:11:47 -0700

    Connecting AJAX call

--- client/public/js/scripts.js
+++ client/public/js/scripts.js
@@ -1,3 +1,34 @@
 $(function() {
-  console.log('scripts loaded...');
+  window.app = {
+    // ----------
+    init: function () {
+      this.handleSearch();
+    },
+
+    // ----------
+    handleSearch: function () {
+      var self = this;
+      var results;
+      this.$searchQuery = document.querySelector('.search-input');
+      this.$searchButton = document.querySelector('.search-button');
+      this.$searchForm = document.querySelector('.video-game-search');
+
+      $(this.$searchForm).on('submit', function(e) {
+        var query = self.$searchForm[0].value;
+        console.warn(query);
+        e.preventDefault();
+        $.ajax({
+          query: query,
+          type:'GET',
+          url: '/getVideoGameData',
+          success: function(res) {
+            console.log(res);
+          }
+        })
+      })
+    }
+  }
+
+  app.init();
+
 });
--- index.js
+++ index.js
@@ -5,14 +5,17 @@ var dotEnv          = require('dotenv').config(),
     bodyParser      = require('body-parser'),
     cookieParser    = require('cookie-parser'),
     app             = express(),
-    indexRouter     = require('./server/routes/index.js'),
     apiAuthRouter   = require('./server/routes/api/auth.js'),
-    apiUsersRouter  = require('./server/routes/api/users.js');
+    apiUsersRouter = require('./server/routes/api/users.js'),
+    path = require('path'),
+    ejsLayouts = require('express-ejs-layouts'),
+    igdb = require('igdb-api-node').default;
+
 
 // connect to db
 // process.env.MONGOLAB_URI is DEPRECATED
 // process.env.MONGODB_URI is needed for when we deploy to Heroku
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/auth_template_app" );
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/start-select-db' );
 
 // log requests to STDOUT
 app.use(morgan('dev'));
@@ -23,16 +26,52 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
 
+// set view engine
+app.set('view engine', 'ejs');
+
+// ejs
+app.use(ejsLayouts);
+
 // flash messages, NEEDS express-flash
 // app.use(flash())
 
 // This is how we read the cookies sent over from the browser
 app.use(cookieParser());
 
-// Set static file root folder
-app.use(express.static('client/public'));
+// set static file root folder
+app.use(express.static(__dirname + '/client/public'));
 
-app.use('/', indexRouter);
+// set views folder
+app.set('views', path.join(__dirname, '/server/views'));
+
+// IGDB
+const apiKey = process.env.IGDB_API_KEY;
+const client = igdb(apiKey);
+
+// root view
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
+app.get('/getVideoGameData', function (req, res) {
+  console.log('REQ.BODY = ', req.body);
+  client.games({
+    // search: req.body, // Sends search query
+    fields: 'name',
+    limit: 5, // Limit to 5 results
+    offset: 15 // Index offset for results
+  }).then(response => {
+    console.log(response)
+      res.send(response);
+  }).catch(error => {
+    throw error;
+  });
+});
+
+// API call
+
+
+
 app.use('/api/auth', apiAuthRouter);
 app.use('/api/users', apiUsersRouter);
 
@@ -40,5 +79,5 @@ app.use('/api/users', apiUsersRouter);
 // process.env.PORT is needed for when we deploy to Heroku
 var port = process.env.PORT || 3000;
 app.listen( port, function() {
-  console.log("free tacos at 3000");
+  console.log('Server running on port:3000');
 });
